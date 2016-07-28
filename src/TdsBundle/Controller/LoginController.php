b6cf46ee4fa36178a978380167561e6063732b5b
Gojko Ubiparip <gojko.ubiparip@pool4tool.com>
2016-07-28 11:03:51 +0200

    login page init

---
+++ src/TdsBundle/Controller/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TdsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login")
+     */
+    public function indexAction()
+    {
+        return $this->render('TdsBundle:Default:login.html.twig');
+    }
+}
